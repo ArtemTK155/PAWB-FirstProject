7ec2df5028d71430c46b821d4fdd838dcc23c38b
ArtemTK155 <artem.tkachenkotk@@gmail.com>
2019-11-06 17:31:54 +0000

    CompA e CompB

---
+++ client/src/views/CompA.vue
@@ -0,0 +1,80 @@
+<template>
+  <div>
+    <v-card>
+      <v-container fluid>
+        <v-row>
+          <v-col cols="5">
+            <v-card flat class="ml-5">
+              <v-card-subtitle>CompA</v-card-subtitle>
+              <v-card-title>
+                <v-alert type="success">{{msg}}</v-alert>
+              </v-card-title>
+              <v-card-text>
+                <v-text-field v-model="msg" label="Regular" single-line></v-text-field>
+              </v-card-text>
+            </v-card>
+          </v-col>
+          <v-spacer></v-spacer>
+          <v-col cols="5">
+            <CompB />
+            <!-- <CompB v-bind:propMsg="propMsg" v-on:changedMsg="newMsg" /> -->
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-card>
+  </div>
+</template>
+<script>
+import CompB from "@/views/CompB";
+export default {
+  name: "CompA",
+
+  data() {
+    return {
+      msg: "",
+      propMsg: ""
+    };
+  },
+  methods: {
+    newMsg() {
+     
+     //
+    },
+    sendToCompB() {
+     //
+    }
+  },
+  components: {
+    CompB
+  },
+  /*
+  computed: {
+    sharedMsg: {
+      get() {
+       //
+      },
+      set(val) {
+       //
+      }
+    }
+
+    
+    sharedMsg() {
+      return this.$store.state.msg;
+    }
+    
+  },
+  */
+  watch: {
+    msg() {
+      //
+    }
+  },
+  mounted() {
+   //
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ client/src/views/CompB.vue
@@ -0,0 +1,61 @@
+<template>
+  <div>
+    <v-card flat color="#385F73" dark class="mr-5">
+      <v-card-subtitle>CompB</v-card-subtitle>
+      <v-card-title>
+        <v-alert type="info">{{msg}}</v-alert>
+      </v-card-title>
+      <v-card-text>
+        <v-text-field v-model="msg" label="Regular" single-line></v-text-field>
+      </v-card-text>
+    </v-card>
+  </div>
+</template>
+<script>
+export default {
+  name: "CompB",
+  props: ["propMsg"],
+  data() {
+    return {
+      msg: ""
+    };
+  },
+  methods: {
+    sendToCompA() {
+     //
+    }
+  },
+  /*
+  computed: {
+    sharedMsg: {
+      get() {
+       //
+      },
+      set(val) {
+        //
+      }
+    },
+  
+    sharedMsg() {
+      return this.$store.state.msg;
+    },
+    
+    cmpMsg() {
+      return this.propMsg;
+    }
+  },
+  */
+  watch: {
+    msg() {
+     //
+    }
+  },
+  
+  mounted() {
+    //
+  }
+};
+</script>
+
+<style>
+</style>
